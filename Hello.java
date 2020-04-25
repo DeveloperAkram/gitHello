42d4723ca77a62009275277424b340b1b60ee956
DeveloperAkram <developeraakram@gmail.com>
2020-04-26 02:44:04 +0600

    Added a new method called greeting to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,14 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Vs code is awesome..");
+
+
+        greeting("Akram");
     }
+
+    public static void greeting(String name) {
+        System.out.println("Welcome "+ name);
+    }
+
+
 }
